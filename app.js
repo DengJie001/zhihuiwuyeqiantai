22a92daabc7b2ea87389d1be875f27a52f713d33
unknown <unknown@example.com>
2021-03-22 22:27:21 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,77 @@
+// app.js
+var baseUrl = 'http://localhost:8080/bysj/user/'
+App({
+  onLaunch() {
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        wx.request({
+          url: baseUrl + 'getUserOpenid.do',
+          data: {
+            code: res.code
+            // code: '1111'
+          },
+          method: 'POST',
+          header: {'Content-Type': 'application/x-www-form-urlencoded'},
+          success: function (res) {
+            wx.setStorageSync('openid', res.data.openid);
+            if (res.data.openid) {
+            } else {
+              wx.redirectTo({
+                url: '../userRegister/userRegister',
+              });
+            }
+          },
+          fail: function (res) {
+            console.log(res);
+          }
+        });
+      }
+    });
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          });
+        }
+      }
+    });
+    // 获取系统状态栏信息
+    wx.getSystemInfo({
+        success: e => {
+            this.globalData.StatusBar = e.statusBarHeight;
+            let capsule = wx.getMenuButtonBoundingClientRect();
+            if (capsule) {
+                this.globalData.Custom = capsule;
+                this.globalData.CustomBar = capsule.bottom + capsule.top - e.statusBarHeight;
+            } else {
+                this.globalData.CustomBar = e.statusBarHeight + 50;
+            }
+        }
+    });
+  },
+  globalData: {
+    userInfo: null,
+    header: {
+      'Cookie': ''
+    }
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,78 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    cardCur: 0,
+    swiperList: [{
+      id: 0,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84000.jpg'
+    }, {
+      id: 1,
+        type: 'image',
+        url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84001.jpg',
+    }, {
+      id: 2,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big39000.jpg'
+    }, {
+      id: 3,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg'
+    }, {
+      id: 4,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg'
+    }, {
+      id: 5,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big21016.jpg'
+    }, {
+      id: 6,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big99008.jpg'
+    }],
+  },
+  // 页面跳转
+  goto: function (e) {
+    var url = '';
+    // TODO 完成页面跳转
+    switch(e.currentTarget.dataset.tag) {
+      case '社区公告':
+        url = '../announcement/announcement';
+        break;
+      case '场地申请':
+        url = '../placeApplication/placeApplication';
+        break;
+      case '缴费中心':
+        url = '../payment/payment';
+        break;
+      case '小区活动':
+        url = '../activities/activities';
+        break;
+      case '物业报修':
+        url = '../repair/repair';
+        break;
+      case '投诉建议':
+        url = '../complaint/complaint';
+        break;
+      case '今日油价':
+        url = '../oilPrice/oilPrice';
+        break;
+      case '天气预报':
+        url = '../weather/weather';
+        break;
+      case '小区房产':
+        url = '../houseInfo/houseInfo'
+        break;
+    }
+    wx.navigateTo({
+      url: url,
+    });
+  },
+  // 事件处理函数
+  onLoad() {
+  },
+})
